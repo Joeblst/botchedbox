51dec68e38f8108d8bdaca609cf0d4b47437a33b
Lukas Goessl <lug0964@thi.de>
2024-09-23 08:45:37 +0200

    Refactor to django

---
+++ app/benchmark/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from django.db import models
+
+
+# Create your models here.
+class Result(models.Model):
+    benchmark_id = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    response = models.TextField(blank=True)
+    score = models.IntegerField(default=0)
+    state = models.CharField(max_length=100)
+    duration = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(default=datetime.now)
+
+class Benchmark(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    config = models.TextField(blank=True)
