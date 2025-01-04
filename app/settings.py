73ea34ce8b27c6bb679c4053611b9bdd4390d12c
Lukas Goessl <lug0964@thi.de>
2025-01-04 17:37:35 +0100

    Debug ob, allow all hosts

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9vsruk3_h&aekx8%je(s^89e*51a87d2kzciw69t9fqq*76q(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
